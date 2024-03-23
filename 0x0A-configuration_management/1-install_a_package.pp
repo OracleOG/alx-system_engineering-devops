9f631ef9f21682a6a6a9eb2c0568d90a75e9794a
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2024-03-23 04:19:16 +0100

    change resource type for manifest to #package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
 # Install the Flask package.
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin', '/usr/local/bin'], # Add necessary paths
-  refreshonly => true, # Only run if notified to refresh
+
+# Include the python module to ensure Python is available
+include python
+
+# Define a class for installing flask with pip3
+class { 'flask_install':
+  # Ensure flask package is installed with pip3 and specify version
+  package { 'flask':
+    ensure => installed,
+    provider => 'pip3',
+    require => Class['python'],
+    version => '2.1.0',
+  }
 }
