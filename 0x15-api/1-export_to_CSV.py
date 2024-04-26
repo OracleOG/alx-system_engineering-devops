81e348b9efaa7be88c694dd8d2565ddf755875ae
OracleOG <emmanuelnwafor212@gmail.com>
2024-04-26 14:20:43 +0100

    task 1 & 2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' Script that uses rest API to return a todo list'''
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = int(sys.argv[1])
+    main_url = f"{url}todos?userId={user_id}"
+    user_url = f"{url}users?id={user_id}"
+
+    response = requests.get(main_url)
+    response2 = requests.get(user_url)
+    response = response.json()
+    response2 = response2.json()
+    comp_todo = []
+
+    for todo in response:
+        if todo['completed']:
+            comp_todo.append(todo)
+    total_todo = len(response)
+    completed = len(comp_todo)
+
+    name = None
+    for i in response2:
+        if i['id'] == user_id:
+            name = i['name']
+
+    filename = f'{user_id}.csv'
+
+    with open(filename, 'w+') as f:
+        for i in response:
+            f.write(f'"{i["userId"]}","{name}",'
+                    f'"{i["completed"]}","{i["title"]}"')
---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+''' Script that uses rest API to return a todo list'''
+import requests
+import sys
+import json
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = int(sys.argv[1])
+    main_url = f"{url}todos?userId={user_id}"
+    user_url = f"{url}users?id={user_id}"
+
+    response = requests.get(main_url)
+    response2 = requests.get(user_url)
+    response = response.json()
+    response2 = response2.json()
+
+    name = None
+    for i in response2:
+        if i['id'] == user_id:
+            name = i['name']
+
+    filename = f'{user_id}.json'
+
+    formatted_data = {
+            user_id: [{"task": i["title"],
+                            "completed": i["completed"],
+                            "username": name}
+                            for i in response if i['userId'] == user_id]
+        }
+    print(formatted_data)
+    json_string = json.dumps(formatted_data, indent=4)
+    print()
+    print()
+    print(json_string)
+
+    with open(filename, 'w+') as f:
+        f.write(json_string)
