1824bca03f434faecec77ee7a4cac20201346d74
OracleOG <emmanuelnwafor212@gmail.com>
2024-05-07 23:51:42 +0100

    print subreddit posts

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of total subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user_agent':'Mozilla/5.0'}
+    headers = {'user_agent': 'Mozilla/5.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
@@ -17,5 +17,4 @@ def number_of_subscribers(subreddit):
         return 0
     if 'subscribers' not in data.get('data'):
         return 0
-    return data['data']['subscribers'] 
-
+    return data['data']['subscribers']
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines how API calls are made and maintained"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts listed
+    for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'user_agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+    data = response.json()
+    posts = data['data']['children']
+    for post in posts:
+        print(post['data']['title'])
